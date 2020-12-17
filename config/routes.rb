005846a1b50be57a13e405ba34fe20c7d854edb4
ryuki29 <ryuki.061529@gmail.com>
2020-12-18 07:37:57 +0900

    routes.rbのrootパスを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'books/index'
+  root 'books#index'
   get 'books/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
