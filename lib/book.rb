124efe1e121872a705898e7427fe5cf47ec1dfb4
ryuki29 <ryuki.061529@gmail.com>
2021-01-12 12:39:56 +0900

    BooksApiモジュール削除
    modelに定義したため

--- lib/book.rb
+++
@@ -1,7 +0,0 @@
-module BooksApi
-  extend self
-  def get_url(keyword)
-      url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=JP&maxResults=30")
-      response = HTTParty.get(url)
-  end
-end
