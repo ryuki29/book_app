b9ac7ed9508640af2939ec9d51f96424f876816a
ryuki29 <ryuki.061529@gmail.com>
2021-01-12 12:39:08 +0900

    検索機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,5 +6,20 @@ class BooksController < ApplicationController
   end
 
   def search
+    @books = []
+    @total_items = 0
+    @keyword = params[:keyword]
+    # @page = params[:page]
+
+    return if params[:keyword].empty?
+
+    result = Book.search_books(@keyword)
+
+    @total_items = result['totalItems']
+
+    return if @total_items.zero?
+
+    @total_items = 100 if @total_items > 100
+    @books = Book.set_search_result(result, @books)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,31 @@
 class Book < ApplicationRecord
+  has_one :review
+  has_many :user_books
+  has_many :users
+  
+  def self.search_books(keyword)
+    encoded_uri = Addressable::URI.encode(
+      "https://www.googleapis.com/books/v1/volumes?q=#{keyword}"
+    )
+    parsed_uri = URI.parse(encoded_uri)
+    JSON.parse(Net::HTTP.get(parsed_uri))
+  end
+
+  def self.set_search_result(result, books)
+    result['items'].each do |item|
+      uid = item['id']
+      title = item['volumeInfo']['title'] ||= ''
+      authors = item['volumeInfo']['authors'] ||= []
+      image_url = item['volumeInfo']['imageLinks'] ? item['volumeInfo']['imageLinks']['thumbnail'].sub(/http/, 'https') : 'book-default.png'
+
+      book = {
+        uid: uid,
+        title: title,
+        authors: authors.join(', '),
+        image_url: image_url
+      }
+      books << book
+    end
+    books
+  end
 end
