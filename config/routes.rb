8aea3158967a1544aaf105577f061807691c2229
ryuki29 <ryuki.061529@gmail.com>
2021-02-18 08:49:33 +0900

    マイページを実装するためにresourcesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks"}
   root 'books#index'
 
+  resources :users, only: :show
+
   resources :books do
     collection do
       get 'search'
