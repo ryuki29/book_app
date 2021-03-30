08ba02b311025b7efbdbf2019883671236a798b0
ryuki29 <ryuki.061529@gmail.com>
2021-03-30 10:44:53 +0900

    外部キーを「reviews_id」から「review_id」へ変更

--- db/migrate/20210130125505_create_action_plans.rb
+++ db/migrate/20210130125505_create_action_plans.rb
@@ -2,7 +2,7 @@ class CreateActionPlans < ActiveRecord::Migration[6.0]
   def change
     create_table :action_plans do |t|
       t.string :plan
-      t.references :reviews, null: false, foreign_key: true
+      t.references :review, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210130130955_create_impressive_phrases.rb
+++ db/migrate/20210130130955_create_impressive_phrases.rb
@@ -2,7 +2,7 @@ class CreateImpressivePhrases < ActiveRecord::Migration[6.0]
   def change
     create_table :impressive_phrases do |t|
       t.text :phrase
-      t.references :reviews, null: false, foreign_key: true
+      t.references :review, null: false, foreign_key: true
 
       t.timestamps
     end
