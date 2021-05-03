7ab95bd5e1b861868f91038a597180a547ed44b8
ryuki29 <ryuki.061529@gmail.com>
2021-05-04 07:23:20 +0900

    マイページの修正#11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @status = params[:status] ? params[:status].to_i : 0
     @user = User.find(params[:id])
-    @username = @user.username
-    @reviews = Review.all
   end
 end
