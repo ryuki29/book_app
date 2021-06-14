1317e0fbda071a87aa4ed72e67b249415d4968fe
ryuki29 <ryuki.061529@gmail.com>
2021-06-14 15:01:23 +0900

    スレッド一覧機能を実装 #65

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show update] do
     member do
       get :following, :followers
+      get :boards
     end
     get :likes, on: :collection
   end
