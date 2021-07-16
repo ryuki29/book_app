42bc420f59a5719faa41d119c20943dd1c8a2a1e
ryuki29 <ryuki.061529@gmail.com>
2021-07-16 11:58:52 +0900

    production.rbを修正 #116

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,10 @@ set :repo_url, "git@github.com:ryuki29/book_app.git"
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :branch, ENV['BRANCH'] || "master"
 
-set :nginx_config_name, "#{fetch(:application)}.conf"
-set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
+set(:bundle_without, '')
+
+# set :nginx_config_name, "#{fetch(:application)}.conf"
+# set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
 
 append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
