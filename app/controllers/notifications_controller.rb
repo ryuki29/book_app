50163b8cef1138910e6e7cc226e9edb0d192f7bd
ryuki29 <ryuki.061529@gmail.com>
2021-08-17 19:45:02 +0900

    update_attributesメソッドをupdateメソッドに変更 #149

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.includes(:visitor, :visited)
     @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+      notification.update(checked: true)
     end
   end
 
