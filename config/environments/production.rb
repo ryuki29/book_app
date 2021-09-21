31e6397fe84a5101be56e870b4a132a105c79492
ryuki29 <ryuki.061529@gmail.com>
2021-09-22 08:45:47 +0900

    ログローテーションを設定 #183

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
+  config.logger = Logger.new("log/production.log", 'weekly')
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
